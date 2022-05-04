f6f37b4857a6910c973479301c1957b12d505689
Leo Capmartin <leo.capmartin@epita.fr>
2022-05-04 14:56:34 +0200

    perlin noise fully working

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include "perlin.h"
 #include <math.h>
 
+#define W_WIDTH 700
+#define W_HEIGHT 700
+
 SDL_Surface *terrain;
 Uint8 r = 0;
 void update(simulation *sim)
@@ -15,24 +18,26 @@ void update(simulation *sim)
 int main()
 {
     srand(time(NULL));
-    terrain = SDL_CreateRGBSurface(0, 500, 500, 32, 0, 0, 0 ,0);
+    terrain = SDL_CreateRGBSurface(0, W_WIDTH, W_HEIGHT, 32, 0, 0, 0 ,0);
 
-    init_gradients(500, 500);
-    for(int x = 0; x < 500; x++)
+    init_gradients(W_WIDTH, W_HEIGHT);
+    float xoff = 0.0;
+    for(int x = 0; x < W_WIDTH; x++)
     {
-        for (int y = 0; y < 500; y++)
+        float yoff = 0.0;
+        for (int y = 0; y < W_HEIGHT; y++)
         {
-            float p = (perlin(x, y) + 1) / 2;
+            float p = (perlin(xoff, yoff) + 1) / 2;
             int r = floorf(p * 255.0);
-            printf("%d\n", r);
             Uint32 color = SDL_MapRGB(terrain->format, r, r, r);
             put_pixel(terrain, x, y, color);
+            yoff += 0.01;
         }
-        
+        xoff += 0.01;
     }
     free_gradients();
 
-    simulation *sim = initEngine(500, 500);
+    simulation *sim = initEngine(W_WIDTH, W_HEIGHT);
 
     run(sim, update);
 
--- perlin.c
+++ perlin.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static int SEED = 0;
+
+static int hash[] = { 151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 
+                      103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 
+                      26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 
+                      87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 
+                      77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 
+                      46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 
+                      187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 
+                      198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 
+                      255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 
+                      170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 
+                      172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 
+                      104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 
+                      241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 
+                      157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 
+                      93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180 };
+
+
+
 typedef struct
 {
     float x, y;
@@ -13,6 +33,11 @@ int cols;
 float interpolate(float a0, float a1, float w)
 {
     return (a1 - a0) * w + a0;
+}
+
+float smooth_inter(float a0, float a1, float w)
+{
+    return interpolate(a0, a1, w * w * (3-2*w));
 }
 
 void init_gradients(int w, int h)
@@ -45,13 +70,19 @@ float dotGradient(int ix, int iy, float x, float y)
     return dx*gradient.x + dy*gradient.y;
 }
 
+int noise2(int x, int y)
+{
+    int tmp = hash[(y + SEED) % 256];
+    return hash[(tmp + x) % 256];
+}
+
 float perlin(float x, float y)
 {
     int x0 = (int)floorf(x);
     int x1 = x0 + 1;
     int y0 = (int)floorf(y);
     int y1 = y0 + 1;
-
+    
     float sx = x - (float)x0;
     float sy = y - (float)y0;
 
@@ -59,12 +90,12 @@ float perlin(float x, float y)
 
     n0 = dotGradient(x0, y0, x, y);
     n1 = dotGradient(x1, y0, x, y);
-    ix0 = interpolate(n0, n1, sx);
+    ix0 = smooth_inter(n0, n1, sx);
 
     n0 = dotGradient(x0, y1, x, y);
     n1 = dotGradient(x1, y1, x, y);
-    ix1 = interpolate(n0, n1, sx);
+    ix1 = smooth_inter(n0, n1, sx);
 
-    value = interpolate(ix0, ix1, sy);
+    value = smooth_inter(ix0, ix1, sy);
     return value;
 }
