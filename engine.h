eeedd66680f92ac7be623c2c12b6f17bcc866cc6
Leo Capmartin <leo.capmartin@epita.fr>
2022-06-13 17:34:48 +0200

    change button design and add stop button

--- engine.h
+++ engine.h
@@ -9,16 +9,18 @@ typedef struct button
     int y;
     int w;
     int h;
-    Uint32 color;
+    SDL_Surface *image;
 } button;
 
 typedef struct simulation
 {
     SDL_Surface *screen;
+    SDL_Surface *renderer;
     struct agentLinkedList* agentList;
     int popCount; //this isn't the current population, it is used to determine the id of every agent. It will only increase.
     button play;
     button speed;
+    button stop;
     int tickInterval;
     int isPaused;
 } simulation;
--- pixelOp.h
+++ pixelOp.h
@@ -2,8 +2,10 @@
 #define PIXELOP_H
 
 #include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
 
 void put_pixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
 Uint32 get_pixel(SDL_Surface *surface, unsigned x, unsigned y);
+SDL_Surface* load_image(char *path);
 
 #endif
